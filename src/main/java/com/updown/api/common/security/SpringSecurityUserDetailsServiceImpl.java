338b969985b3ea85ae3ca0d90add5fb2ce3c8f6f
updown-90 <sangha.kim@kurlycorp.com>
2023-02-02 17:34:40 +0900

    refactor: 주석 추가

--- src/main/java/com/updown/api/common/security/SpringSecurityUserDetailsServiceImpl.java
+++ src/main/java/com/updown/api/common/security/SpringSecurityUserDetailsServiceImpl.java
@@ -21,8 +21,9 @@ public class SpringSecurityUserDetailsServiceImpl implements UserDetailsService 
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
 
-        // 이쪽에 디비에 만들어놓음 사용자 아이디 조회해서 있는지 없는지 보고 SpringSecurity UserDetails implements 받아서 메소드 오버라이드 해놓은은
-        // 클래스 인스턴스화 해서 리턴하면 로그인 끝
+        // 이쪽에 디비에 만들어놓음 사용자 아이디 조회해서 있는지 없는지 보고 SpringSecurity UserDetails implements 받아서 메소드 오버라이드 해놓은
+        // 클래스 인스턴스화 해서 리턴하면 클라이언트의 쿠키에 JSESSIONID라는 키로 SessionID가 저장되기 때문에 한 브라우져 안에서 인증은 계속 유효하고
+        // 브라우져 끄거나 하면 다시 받아야함
         AccountEntity accountEntity = accountEntityRepository.findAccountByLoginId(loginId)
                 .orElseThrow(() -> new CustomRuntimeException(ExceptionType.NOT_FOUND_USER));
 
