0c17699634a3d16dc88d50d37349da0216fc8ce9
updown-90 <sangha.kim@kurlycorp.com>
2023-08-02 16:31:05 +0900

    feat: lombok 사용

--- src/main/java/com/updown/api/account/infrastructure/repository/AccountEntityQueryDSLRepositoryImpl.java
+++ src/main/java/com/updown/api/account/infrastructure/repository/AccountEntityQueryDSLRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.updown.api.account.infrastructure.dto.AccountEntityQueryDSLDTO;
 import com.updown.api.account.infrastructure.dto.QAccountEntityQueryDSLDTO;
 import com.updown.api.account.presentation.dto.request.AccountsFindRequestDTO;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -11,13 +12,10 @@ import java.util.List;
 import static com.updown.api.account.domain.QAccountEntity.accountEntity;
 
 
+@RequiredArgsConstructor
 public class AccountEntityQueryDSLRepositoryImpl implements AccountEntityQueryDSLRepository{
 
     private final JPAQueryFactory queryFactory;
-
-    public AccountEntityQueryDSLRepositoryImpl(EntityManager entityManager) {
-        this.queryFactory = new JPAQueryFactory(entityManager);
-    }
 
     @Override
     public List<AccountEntityQueryDSLDTO> findAccounts(AccountsFindRequestDTO accountsFindRequestDTO) {
